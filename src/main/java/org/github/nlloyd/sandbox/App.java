21cc52af0505882f7ba16661b0eb7c0373fc50cc
Nick Lloyd <nlloyd@gmail.com>
2012-12-14 12:53:24 -0500

    various moving-things-around and adding-things changes
    - README.md has a tiny bit more to it (really just the license)
    - MongoScope.java added
    - sandbox code added/updated
    - tagged release v2.0.8 shell/*.js files from git@github.com:mongodb/mongo.git added to src/main/javascript
    - pom.xml updated with project metadata

---
+++ src/main/java/org/github/nlloyd/sandbox/App.java
@@ -0,0 +1,73 @@
+/**
+ *  Copyright (c) 2012 Nick Lloyd
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package org.github.nlloyd.sandbox;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.tools.shell.Global;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+//	private static Global global = new Global();
+//	
+//	private class ScriptAction implements ContextAction {
+//		
+//		private String script;
+//		
+//		public ScriptAction(String script) {
+//			this.script = script;
+//		}
+//
+//		public Object run(Context cx) {
+//			Script compiled = cx.compileString(this.script, "<script>", 1, null);
+//			compiled.exec(cx, global);
+//			return null;
+//		}
+//		
+//	}
+	
+    public static void main( String[] args ) throws IllegalAccessException, InstantiationException, InvocationTargetException
+    {
+        System.out.println( "Hello World!" );
+        
+//        ContextFactory cxf = new ContextFactory();
+        Context cx = Context.enter();
+        Global global = new Global(cx);
+//        global.init(cxf);
+//        Context jsCx = cxf.enterContext();
+        cx.evaluateString(global, "var hullo = 'Hello World!'", 
+        		"helloWorld.js", 1, null);
+        cx.evaluateString(global, "print(hullo)", 
+        		"helloWorld.js", 2, null);
+        ScriptableObject.defineClass(global, SandboxJS.class);
+        cx.evaluateString(global, 
+        		"var test = new SandboxJS(); test.wtf(); test.wtf; test.notafunc();",
+        		"noSuchMethodTester.js", 1, null);
+        Context.exit();
+    }
+}
