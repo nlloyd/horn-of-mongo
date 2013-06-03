e75158458cb737cda33051a68f2d8d18488eb070
Nick Lloyd <nlloyd@gmail.com>
2013-06-03 09:56:54 -0400

    issue #3 changes
    - MongoScriptAction refactored to detect and handle special shell commands like "show" and "use" as they are handled by the official mongo shell
    - for this functionality to work these commands must be provided as simple string commands with no other content, ex "use testdb"

--- src/main/java/com/github/nlloyd/hornofmongo/action/MongoScriptAction.java
+++ src/main/java/com/github/nlloyd/hornofmongo/action/MongoScriptAction.java
@@ -26,99 +26,165 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
 import java.io.Reader;
+import java.io.StringReader;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ScriptableObject;
 
 import com.github.nlloyd.hornofmongo.MongoScope;
 import com.mongodb.MongoException;
 
 /**
- * A concrete implementation of {@link MongoAction} that wraps a JavaScript script 
- * for execution in either String, Reader, or File form.
+ * A concrete implementation of {@link MongoAction} that wraps a JavaScript
+ * script for execution in either String, Reader, or File form.
  * 
  * @author nlloyd
- *
+ * 
  */
 public class MongoScriptAction extends MongoAction {
-	
-	protected String scriptName = "anon";
-	protected String scriptString = null;
-	protected Reader scriptReader = null;
-	
-	public MongoScriptAction(MongoScope mongoScope, String script) {
-		super(mongoScope);
-		this.scriptString = script;
-	}
-	
-	public MongoScriptAction(MongoScope mongoScope, File script) {
-		super(mongoScope);
-		this.scriptName = script.getName();
-		try {
-			this.scriptReader = new BufferedReader(new FileReader(script));
-		} catch (FileNotFoundException e) {
-			throw new MongoException("Attempted to execute non-existent file " + script.getAbsolutePath(), e);
-		}
-	}
-	
-	public MongoScriptAction(MongoScope mongoScope, Reader script) {
-		super(mongoScope);
-		this.scriptReader = script;
-	}
-	
-	public MongoScriptAction(MongoScope mongoScope, String name, String script) {
-		super(mongoScope);
-		this.scriptName = name;
-		this.scriptString = script;
-	}
-	
-	public MongoScriptAction(MongoScope mongoScope, String name, File script) {
-		super(mongoScope);
-		this.scriptName = name;
-		try {
-			this.scriptReader = new BufferedReader(new FileReader(script));
-		} catch (FileNotFoundException e) {
-			throw new MongoException("Attempted to execute non-existent file " + script.getAbsolutePath(), e);
-		}
-	}
-	
-	public MongoScriptAction(MongoScope mongoScope, String name, Reader script) {
-		super(mongoScope);
-		this.scriptName = name;
-		this.scriptReader = script;
-	}
-	
-	/**
-	 * Uses the provided context to execute either a String script, 
-	 * @see org.mozilla.javascript.ContextAction#run(org.mozilla.javascript.Context)
-	 */
-	@Override
-	public Object run(Context cx) {
-//		cx.setOptimizationLevel(-1);
-		Object result = null;
-		if(StringUtils.isNotBlank(scriptString)) {
-			result =  cx.evaluateString(
-					mongoScope, 
-					scriptString,
-					scriptName, 
-					0, 
-					null);
-		} else if(scriptReader != null) {
-			try {
-				result =  cx.evaluateReader(
-						mongoScope, 
-						scriptReader,
-						scriptName, 
-						0, 
-						null);
-			} catch (IOException e) {
-				throw new MongoException("IOException when executing a script in Reader form.", e);
-			}
-		}
-		// TODO throw exception if nothing to execute?
-		
-		return result;
-	}
+
+    protected String scriptName = "anon";
+    protected String scriptString = null;
+    protected Reader scriptReader = null;
+
+    public MongoScriptAction(MongoScope mongoScope, String script) {
+        super(mongoScope);
+        this.scriptString = script;
+    }
+
+    public MongoScriptAction(MongoScope mongoScope, File script) {
+        super(mongoScope);
+        this.scriptName = script.getName();
+        try {
+            this.scriptReader = new BufferedReader(new FileReader(script));
+        } catch (FileNotFoundException e) {
+            throw new MongoException("Attempted to execute non-existent file "
+                    + script.getAbsolutePath(), e);
+        }
+    }
+
+    public MongoScriptAction(MongoScope mongoScope, Reader script) {
+        super(mongoScope);
+        this.scriptReader = script;
+    }
+
+    public MongoScriptAction(MongoScope mongoScope, String name, String script) {
+        super(mongoScope);
+        this.scriptName = name;
+        this.scriptString = script;
+    }
+
+    public MongoScriptAction(MongoScope mongoScope, String name, File script) {
+        super(mongoScope);
+        this.scriptName = name;
+        try {
+            this.scriptReader = new BufferedReader(new FileReader(script));
+        } catch (FileNotFoundException e) {
+            throw new MongoException("Attempted to execute non-existent file "
+                    + script.getAbsolutePath(), e);
+        }
+    }
+
+    public MongoScriptAction(MongoScope mongoScope, String name, Reader script) {
+        super(mongoScope);
+        this.scriptName = name;
+        this.scriptReader = script;
+    }
+
+    /**
+     * Uses the provided context to execute either a String script,
+     * 
+     * @see org.mozilla.javascript.ContextAction#run(org.mozilla.javascript.Context)
+     */
+    @Override
+    public Object run(Context cx) {
+        // cx.setOptimizationLevel(-1);
+        Object result = null;
+        String script;
+        if (StringUtils.isNotBlank(scriptString))
+            script = scriptString;
+        else {
+            try {
+                script = readFully(scriptReader);
+            } catch (IOException e) {
+                throw new MongoException(
+                        "IOException when executing a script in Reader form.",
+                        e);
+            }
+        }
+
+        // this could be a command like "use", "show", or "help" which must be a
+        // one-liner without a semi-colon line terminator
+        boolean wasCmd = false;
+        if (isOneLine(script)) {
+            script.trim();
+            String cmd = script;
+            if (cmd.indexOf(' ') > 0)
+                cmd = script.substring(0, cmd.indexOf(' '));
+            if (cmd.indexOf('"') == -1) {
+                StringBuilder cmdCheck = new StringBuilder();
+                cmdCheck.append("__iscmd__ = shellHelper[\"");
+                cmdCheck.append(cmd);
+                cmdCheck.append("\"];");
+                cx.evaluateString(mongoScope, cmdCheck.toString(),
+                        "(shellhelp1)", 0, null);
+                if (Context.toBoolean(ScriptableObject.getTopScopeValue(
+                        mongoScope, "__iscmd__"))) {
+                    StringBuilder cmdScript = new StringBuilder();
+                    cmdScript.append("shellHelper( \"");
+                    cmdScript.append(cmd);
+                    cmdScript.append("\" , \"");
+                    cmdScript.append(script.substring(cmd.length()));
+                    cmdScript.append("\");");
+                    cx.evaluateString(mongoScope, cmdScript.toString(),
+                            "(shellhelp2)", 0, null);
+                    wasCmd = true;
+                }
+            }
+        }
+
+        if (!wasCmd) {
+            result = cx.evaluateString(mongoScope, script, scriptName, 0, null);
+        }
+
+        return result;
+    }
+
+    /**
+     * Borrowed from Rhino's Parser class.
+     * 
+     * @param reader
+     * @return
+     * @throws IOException
+     */
+    private String readFully(Reader reader) throws IOException {
+        BufferedReader in = new BufferedReader(reader);
+        try {
+            char[] cbuf = new char[1024];
+            StringBuilder sb = new StringBuilder(1024);
+            int bytes_read;
+            while ((bytes_read = in.read(cbuf, 0, 1024)) != -1) {
+                sb.append(cbuf, 0, bytes_read);
+            }
+            return sb.toString();
+        } finally {
+            in.close();
+        }
+    }
+
+    private boolean isOneLine(final String script) {
+        LineNumberReader lnr = new LineNumberReader(new StringReader(script));
+        try {
+            @SuppressWarnings("unused")
+            int lastRead;
+            while ((lastRead = lnr.read()) != -1) {
+            }
+        } catch (IOException e) {
+        }
+        return lnr.getLineNumber() == 1;
+    }
 
 }
